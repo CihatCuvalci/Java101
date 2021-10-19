4bec614bfd845ec8c2902bde54ea5e8a88e2a6ef
Cihat Çuvalcı <77547164+CihatCuvalci@users.noreply.github.com>
2021-10-19 15:06:52 +0300

    Update BelirliBirSayıyaKadarÜsBulma.java

--- BelirliBirSayıyaKadarÜsBulma.java
+++ BelirliBirSayıyaKadarÜsBulma.java
@@ -1,14 +1,20 @@
 import java.util.Scanner;
 
-public class J92ninKuvvetleriniBulma {
+public class J9a2ninKuvvetleriniBulma {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
         System.out.println("Bir Sayı Giriniz: ");
-        int power = 1, n = inp.nextInt();
+        int power4 = 1, power5 = 1, n = inp.nextInt();
 
-        while (power < n){
-            System.out.println(power);
-            power *= 2;
+        while (power4 < n){
+            System.out.print(power4 + ",");
+            power4 *= 4;
         }
+        System.out.println();
+        while (power5 < n){
+            System.out.print(power5 + ",");
+            power5 *= 5;
+        }
+
     }
 }
